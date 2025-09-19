e86e1e78f74cf3f094fb2b08a822341551957787
Khoi <leminhkhoi1440@gmail.com>
2025-09-18 20:25:07 -0400

    Revert "Update HelloWorld.java"
    This reverts commit d7b0293fcce71d5914ea7b224835f94887d901ff.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,11 +2,6 @@ public class HelloWorld
 {
    public static void main(String[] args)
    {
-      String name = "Khoi Le";
-      int age = 21;
-      
-      System.out.println("Hello World!");
-      System.out.println(name);
-      System.out.println(age);
+      System.out.println("Hello World");
    }
 }
